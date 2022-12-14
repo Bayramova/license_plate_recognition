44bfc9092749f71b195a24dbde727e745816f68c
Sabina Bayramova <sb.sabina00@gmail.com>
2022-12-14 18:44:41 +0300

    Add streamlit app

--- src/license_plate_recognition/recognize.py
+++ src/license_plate_recognition/recognize.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from typing import Union
 
 import click
 import cv2
+import numpy as np
+import numpy.typing as npt
 import torch
 import torchvision.transforms as transforms
 
@@ -28,31 +31,47 @@ from license_plate_recognition.trainer import OCRModule
 def recognize(ckpt_path: Path, img_path: Path) -> None:
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    dictionary = utils.Dictionary()
-    for char in (
-        alphabet.blank + alphabet.provinces + alphabet.alphabets + alphabet.numbers
-    ):
-        dictionary.add_char(char)
+    converter = load_label_converter()
 
-    converter = utils.LabelConverter(dictionary=dictionary)
-
-    # load the model
-    checkpoint = torch.load(ckpt_path, map_location=device)
-    hyper_parameters = checkpoint["hyper_parameters"]
-    model = OCRModule(**hyper_parameters, label_converter=converter)
-    model_weights = checkpoint["state_dict"]
-    model.load_state_dict(model_weights)
+    model = load_model(ckpt_path=ckpt_path, device=device, label_converter=converter)
     model.eval()
 
     # preprocess image
     img = cv2.imread(img_path)
-    img = cv2.resize(img, (128, 32))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    transform = transforms.ToTensor()
-    img = transform(img)
+    img = preprocess_image(img)
 
     # make predicition
     with torch.no_grad():
         pred = model(img.unsqueeze(0))
 
     click.echo(f"Predicted label: {converter.decode(pred.argmax(-1))}")
+
+
+def preprocess_image(img: npt.NDArray[np.int_]) -> torch.Tensor:
+    img = cv2.resize(img, (128, 32))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    transform = transforms.ToTensor()
+    return transform(img)
+
+
+def load_label_converter() -> utils.LabelConverter:
+    dictionary = utils.Dictionary()
+    for char in (
+        alphabet.blank + alphabet.provinces + alphabet.alphabets + alphabet.numbers
+    ):
+        dictionary.add_char(char)
+
+    return utils.LabelConverter(dictionary=dictionary)
+
+
+def load_model(
+    label_converter: utils.LabelConverter,
+    ckpt_path: Union[Path, str] = "checkpoints/best_checkpoint.ckpt",
+    device: Union[torch.device, str] = "cpu",
+) -> OCRModule:
+    checkpoint = torch.load(ckpt_path, map_location=device)
+    hyper_parameters = checkpoint["hyper_parameters"]
+    model = OCRModule(**hyper_parameters, label_converter=label_converter)
+    model_weights = checkpoint["state_dict"]
+    model.load_state_dict(model_weights)
+    return model
---
+++ src/streamlit_app.py
@@ -0,0 +1,45 @@
+# type: ignore
+
+import numpy as np
+from PIL import Image
+import streamlit as st
+import torch
+
+from license_plate_recognition.recognize import (
+    load_label_converter,
+    load_model,
+    preprocess_image,
+)
+
+
+@st.cache
+def cached_label_converter():
+    label_converter = load_label_converter()
+    return label_converter
+
+
+@st.cache(allow_output_mutation=True)
+def cached_model(label_converter):
+    model = load_model(label_converter)
+    model.eval()
+    return model
+
+
+label_converter = cached_label_converter()
+model = cached_model(label_converter)
+
+st.title("License Plate Recognition")
+
+uploaded_file = st.file_uploader("Choose an image:", type="jpg")
+
+if uploaded_file:
+    img = Image.open(uploaded_file)
+    st.image(img, channels="BGR")
+
+    img = preprocess_image(np.array(img))
+
+    with torch.no_grad():
+        pred = model(img.unsqueeze(0))
+
+    st.write("Prediction:")
+    st.success(f"{label_converter.decode(pred.argmax(-1))}")
